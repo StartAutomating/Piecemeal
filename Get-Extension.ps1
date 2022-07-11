7a0743a1e417835824d0bac966cb8c78f593c38d
James Brundage <@github.com>
2022-07-11 11:47:37 -0700

    Get-Extension:  Fixing .GetHelpField (#100)

--- Get-Extension.ps1
+++ Get-Extension.ps1
@@ -315,15 +315,21 @@
 
             $extCmd.PSObject.Methods.Add([psscriptmethod]::New('GetHelpField', {
                 param([Parameter(Mandatory)]$Field)
-                foreach ($block in $this.BlockComments) {
+                $fieldNames = 'synopsis','description','link','example','inputs', 'outputs', 'parameter', 'notes'
+                foreach ($block in $this.BlockComments) {                
                     foreach ($match in [Regex]::new("
                         \.(?<Field>$Field)                   # Field Start
                         [\s-[\r\n]]{0,}                      # Optional Whitespace
                         [\r\n]+                              # newline
-                        (?<Content>(.|\s)+?(?=(\.\w+|\#\>))) # Anything until the next .\field or end of the comment block
+                        (?<Content>(?:.|\s)+?(?=
+                        (
+                            [\r\n]{0,}\s{0,}\.(?>$($fieldNames -join '|'))|
+                            \#\>|
+                            \z
+                        ))) # Anything until the next .field or end of the comment block
                         ", 'IgnoreCase,IgnorePatternWhitespace', [Timespan]::FromSeconds(1)).Matches(
                             $block.Value
-                        )) {
+                        )) {                        
                         $match.Groups["Content"].Value -replace '[\s\r\n]+$'
                     }                    
                 }

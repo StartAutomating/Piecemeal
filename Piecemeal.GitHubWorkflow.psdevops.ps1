cbe142aeec3501277349dbb200dc6af6c358032b
James Brundage <@github.com>
2022-11-14 22:26:49 -0800

    Adding GitPub Support (Fixes #111)

--- Piecemeal.GitHubWorkflow.psdevops.ps1
+++ Piecemeal.GitHubWorkflow.psdevops.ps1
@@ -1,9 +1,16 @@
 ﻿#requires -Module PSDevOps
+#requires -Module GitPub
+
 Push-Location $PSScriptRoot
-New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish |
+
+New-GitHubWorkflow -Name "Analyze, Test, Tag, and Publish" -On Push, PullRequest, Demand -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, RunHelpOut |
     Set-Content .\.github\workflows\TestAndPublish.yml -Encoding UTF8 -PassThru
 
-New-GitHubWorkflow -Name "Run HelpOut" -On Push  -Job HelpOut |
+New-GitHubWorkflow -Name "Run HelpOut" -On Push  -Job RunHelpOut |
     Set-Content .\.github\workflows\UpdateDocs.yml -Encoding UTF8 -PassThru
+
+Import-BuildStep -ModuleName GitPub
+
+New-GitHubWorkflow -On Issue, Demand -Job RunGitPub -Name "GitPub" -OutputPath .\.github\workflows\GitPub.yml
 
 Pop-Location
